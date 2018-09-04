06b232b9469431c02a2d435ddd41c5ffadbacd22
unknown <liuyl3@staff.ifeng.com>
2018-09-04 16:22:27 +0800

    wrap_flow undifined ' windowMatchingMask' debug

--- src/dense_warp_flow_gpu.cpp
+++ src/dense_warp_flow_gpu.cpp
@@ -6,12 +6,12 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+//#include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/features2d/features2d.hpp"
 #include "opencv2/core/core.hpp"
 #include "opencv2/nonfree/nonfree.hpp"
 #include "opencv2/gpu/gpu.hpp"
-#include "opencv2/gpu/gpu.hpp"
+//#include "opencv2/gpu/gpu.hpp"
 
 #include <stdio.h>
 #include <iostream>
